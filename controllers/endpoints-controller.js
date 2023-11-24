c38d06b0538ed1374dd414404fa13c120ad7edb4
Renato Cipolla <renato.cipolla94@gmail.com>
2023-11-24 15:14:55 +0000

    removal of unnecessary console log

--- controllers/endpoints-controller.js
+++ controllers/endpoints-controller.js
@@ -1,7 +1,6 @@
 const { getAllEndpoints } = require("../models/endpoints-model")
 
 exports.getEndpoints = (req, res, next) => {
-    console.log('hello from endpoints controller')
     const endpoints = getAllEndpoints()
     res.status(200).send({ endpoints })
 }
